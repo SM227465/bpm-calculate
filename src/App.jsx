e460e07e3069acb4c5263bb0df9a680035b8488e
Sourav Mandal <sm227465@gmail.com>
2023-06-11 21:03:58 +0530

    bpm calculate using music-tempo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import MusicTempo from 'music-tempo';
+import { Audio } from 'react-loading-icons';
 import { useState } from 'react';
-import { analyzeFullBuffer } from 'realtime-bpm-analyzer';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [bpm, setBpm] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const resetHandler = () => {
+    setBpm(0);
+    const file = document.getElementById('fileInput');
+
+    if (file) {
+      file.value = '';
+    }
+  };
 
   const fileChangeHandler = (event) => {
+    setIsLoading(true);
+
     const file = event.target.files[0];
     const reader = new FileReader();
 
@@ -13,8 +26,22 @@ function App() {
       const audioCtx = new AudioContext();
 
       audioCtx.decodeAudioData(reader.result, async (audioBuffer) => {
-        const topCandidates = await analyzeFullBuffer(audioBuffer);
-        console.log(topCandidates);
+        const audioData = [];
+
+        if (audioBuffer.numberOfChannels === 2) {
+          const channel1Data = audioBuffer.getChannelData(0);
+          const channel2Data = audioBuffer.getChannelData(1);
+          const length = channel1Data.length;
+          for (let i = 0; i < length; i++) {
+            audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
+          }
+        } else {
+          audioData = audioBuffer.getChannelData(0);
+        }
+
+        const mt = new MusicTempo(audioData);
+        setBpm(parseInt(mt.tempo));
+        setIsLoading(false);
       });
     });
 
@@ -22,9 +49,16 @@ function App() {
   };
 
   return (
-    <div>
-      <h2>BMP</h2>
+    <div className='container'>
+      <h2>To find BMP of a song please select an audio file below</h2>
       <input type='file' id='fileInput' accept='audio/*' onChange={fileChangeHandler} />
+      <div>
+        <h5>BPM: {bpm}</h5>
+        {isLoading && <Audio fill='#06bcee' stroke='#98ff98' strokeOpacity={0.125} speed={0.75} />}
+      </div>
+      <button disabled={bpm === 0} onClick={resetHandler}>
+        Reset
+      </button>
     </div>
   );
 }
